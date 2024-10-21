f7b59ff0f32c0fbbf4787b0f438cfc71dc79f37f
AndyZam99 <77559233+AndyZam99@users.noreply.github.com>
2024-10-21 11:18:58 -0600

    Update

--- sanity/schemaTypes/banners.ts
+++ sanity/schemaTypes/banners.ts
@@ -1,59 +1,60 @@
+import { Rule } from 'sanity';
+
 export default {
-    name: 'banner',
-    title: 'Banners',
-    type: 'document',
-    fields: [
-      {
-        name: 'titulo',
-        title: 'Título',
-        type: 'string',
-        validation: Rule => Rule.required().min(10).max(100).warning('El título debe tener entre 10 y 100 caracteres.'),
+  name: 'banner',
+  title: 'Banners',
+  type: 'document',
+  fields: [
+    {
+      name: 'titulo',
+      title: 'Título',
+      type: 'string',
+      validation: (Rule: Rule) => Rule.required().min(10).max(100).warning('El título debe tener entre 10 y 100 caracteres.'),
+    },
+    {
+      name: 'slug',
+      title: 'Slug',
+      type: 'slug',
+      options: {
+        source: 'titulo',
+        maxLength: 96,
       },
-      {
-        name: 'slug',
-        title: 'Slug',
-        type: 'slug',
-        options: {
-          source: 'titulo',
-          maxLength: 96,
-        },
+    },
+    {
+      name: 'autor',
+      title: 'Autor',
+      type: 'reference',
+      to: [{ type: 'autor' }],
+      validation: (Rule: Rule) => Rule.required(),
+    },
+    {
+      name: 'contenido',
+      title: 'Contenido',
+      type: 'blockContent',
+    },
+    {
+      name: 'tags',
+      title: 'Tags',
+      type: 'array',
+      of: [{ type: 'string' }],
+      options: {
+        layout: 'tags',
       },
-      {
-        name: 'autor',
-        title: 'Autor',
-        type: 'reference',
-        to: [{ type: 'autor' }],
-        validation: Rule => Rule.required(),
+    },
+    {
+      name: 'fechaPublicacion',
+      title: 'Fecha de Publicación',
+      type: 'datetime',
+      validation: (Rule: Rule) => Rule.required(),
+    },
+    // Campo de imagen
+    {
+      name: 'imagenDestacada',
+      title: 'Imagen Destacada',
+      type: 'image',
+      options: {
+        hotspot: true,
       },
-      {
-        name: 'contenido',
-        title: 'Contenido',
-        type: 'blockContent',
-      },
-      {
-        name: 'tags',
-        title: 'Tags',
-        type: 'array',
-        of: [{ type: 'string' }],
-        options: {
-          layout: 'tags',
-        },
-      },
-      {
-        name: 'fechaPublicacion',
-        title: 'Fecha de Publicación',
-        type: 'datetime',
-        validation: Rule => Rule.required(),
-      },
-      // Campo de imagen
-      {
-        name: 'imagenDestacada',
-        title: 'Imagen Destacada',
-        type: 'image',
-        options: {
-          hotspot: true,
-        },
-      },
-    ],
-  };
-  
+    },
+  ],
+};
